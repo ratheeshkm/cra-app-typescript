a903507227d0a5cd48471566b92336cb232682cc
ratheeshkm <ratheesh.archal@gmail.com>
2021-08-25 20:42:00 +0530

    Added header component

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
+import { Outlet } from 'react-router';
+import { styled } from '@material-ui/core/styles';
+
+import MainNavBar from 'src/components/MainNavBar';
+
+const MainLayoutRoot = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  display: 'flex',
+  height: '100%',
+  overflow: 'hidden',
+  width: '100%',
+}));
 
 const MainLayout = () => {
-  return <div>MAIN LAYOUT</div>;
+  return (
+    <MainLayoutRoot>
+      <MainNavBar />
+      <Outlet />
+    </MainLayoutRoot>
+  );
 };
 
 export default MainLayout;
---
+++ src/components/MainNavBar/index.tsx
@@ -0,0 +1,9 @@
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const MainNavbar = (props: any) => (
+  <AppBar elevation={0} {...props}>
+    <Toolbar sx={{ height: 64 }}>Logo</Toolbar>
+  </AppBar>
+);
+
+export default MainNavbar;
